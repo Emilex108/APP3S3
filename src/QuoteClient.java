21a2e55a0d9bf8c721218fdda824dc9e89ed5a76
Emile <gage1002@usherbrooke.ca>
2021-06-18 15:59:49 -0400

    Added javadoc

--- src/QuoteClient.java
+++ src/QuoteClient.java
@@ -12,7 +12,7 @@ public class QuoteClient {
     private static DatagramSocket socket;
     private static InetAddress address;
     private static int portName = 27841;
-
+    private static int currentPacketNumber = 0;
     private static OutputStream os;
 
     public static void main(String[] args) throws IOException {
@@ -34,18 +34,33 @@ public class QuoteClient {
         socket.close();
     }
 
+    /**
+     * Suit notre protocole maison pour envoyer 1 fichier
+     * @param fileName Nom du fichier à envoyer
+     * @throws IOException Si problème à lire le fichier
+     */
     private static void writeWithProtocol(String fileName) throws IOException {
         //--------------------COUCHE D'APPLICATION
         //Créer un espace mémoire
-        byte[] bufferFichier = new byte[32768];
+        byte[] dataFichier;
         //Charge le fichier
-        bufferFichier = Files.readAllBytes(Paths.get(fileName));
+        dataFichier = Files.readAllBytes(Paths.get(fileName));
         log("Fichier lu.");
         //--------------------COUCHE DE TRANSPORT
+        //Crée un header
+        byte[] dataHeader = createHeader(fileName.getBytes());
+        //Crée le data total avant CRC
+        byte[] dataTotalAvantCRC = new byte[dataHeader.length+fileName.getBytes().length];
+        System.arraycopy(dataHeader, 0, dataTotalAvantCRC, 0, dataHeader.length);
+        System.arraycopy(fileName.getBytes(), 0, dataTotalAvantCRC, dataHeader.length, fileName.getBytes().length);
+        //TODO: Crée le CRC
+
+        //TODO: Ajoute le CRC au data total
+
         //Premier packet contient : Le nom du fichier
+        //TODO: Modifier le packet pour qu'il contienne le bon data
         DatagramPacket packet = new DatagramPacket(fileName.getBytes(), fileName.getBytes().length, address, portName);
         log("Premier packet créé.");
-        //TODO: Ajouter un header ? + CRC
         socket.send(packet);
         log("Premier packet envoyé.");
         //Recevoir la réponse (Acknowledgement)
@@ -54,13 +69,54 @@ public class QuoteClient {
         socket.receive(packet);
         System.out.println(new String(packet.getData(), StandardCharsets.UTF_8));
         log(new String(packet.getData(), StandardCharsets.UTF_8));
+        //TODO: Prendre une décision selon le message de ACK (Renvoyer ou non)
+        //TODO: Même processus total pour le data du fichier
     }
 
+    /**
+     * Permet de sauvegarder des logs
+     * @param msg Message a enregistrer
+     * @throws IOException Si erreur dans le fichier
+     */
     private static void log(String msg) throws IOException {
         // Crée un time stamp assez clean
         os.write(("[" + new Timestamp(System.currentTimeMillis()).toString().substring(11,21) + "] [Client] : ").getBytes());
         os.write(msg.trim().getBytes());
         // Change de ligne
         os.write(10);
+    }
+
+    /**
+     * Crée un header pour un data spécifique
+     * @param data Data à communiquer dans le packet
+     * @return Un header complet avant CRC
+     */
+    private static byte[] createHeader(byte[] data){
+        /**
+         * # de packet  = 1 byte
+         * délimiteur = 1 byte
+         * adresse destination = 6 bytes
+         * adresse source = 6 bytes
+         * taille du message = 1 byte
+         * total = 15 bytes
+         */
+        //TODO: Ajouter place pour CRC
+        byte[] header = new byte[15];
+        //Ajoute le numéro de packet
+        header[0] = (byte) (currentPacketNumber+1);
+        //Ajoute le délimiteur (0) -> Simple car premier packet est 1
+        header[1] = (byte) 01111110;
+        //TODO: S'assurer que le byte est chill, sinon le mettre en decimal et le cast
+        System.out.println(header[1]);
+        for(int i = 2; i < 8; i++){
+            //Check si l'adresse est la même (send/receive)
+            //Sinon remplacer la deuxième assignation par l'adresse locale
+            //TODO: Check si l'adresse contient 6 bytes (Print length) et check si c'est MAC address
+            header[i] = address.getAddress()[i-1];
+            header[i+6] = address.getAddress()[i-1];
+        }
+        //Ajoute la longueur du message dans le header
+        header[15] = (byte) data.length;
+        return header;
     }
 }
